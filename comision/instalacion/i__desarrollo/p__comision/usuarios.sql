fcc181fb31be1a8b7775c5ad698b879e21125c38
Leonardo Fontes <lfontes@fca.uncu.edu.ar>
2024-05-24 13:13:31 -0300

    arreglo imagen en login

---
+++ comision/instalacion/i__desarrollo/p__comision/usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO apex_usuario_proyecto (proyecto, usuario_grupo_acc, usuario, usuario_perfil_datos) VALUES ('comision', 'personal', 'ebermejillo@fca.uncu.edu.ar', NULL);
+INSERT INTO apex_usuario_proyecto (proyecto, usuario_grupo_acc, usuario, usuario_perfil_datos) VALUES ('comision', 'personal', 'emilitello@fca.uncu.edu.ar', NULL);
+INSERT INTO apex_usuario_proyecto (proyecto, usuario_grupo_acc, usuario, usuario_perfil_datos) VALUES ('comision', 'admin', 'lfontes@fca.uncu.edu.ar', NULL);
+INSERT INTO apex_usuario_proyecto (proyecto, usuario_grupo_acc, usuario, usuario_perfil_datos) VALUES ('comision', 'admin', 'toba', NULL);
