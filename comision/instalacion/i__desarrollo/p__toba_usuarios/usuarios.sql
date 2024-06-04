a8341b584c8da1622a5cb3ed9604f34f4117cbfc
Leonardo Fontes <lfontes@fca.uncu.edu.ar>
2024-06-04 10:05:00 -0300

    Quitadas las carpetas de instalacion del raiz

--- comision/instalacion/i__desarrollo/p__toba_usuarios/usuarios.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO apex_usuario_proyecto (proyecto, usuario_grupo_acc, usuario, usuario_perfil_datos) VALUES ('toba_usuarios', 'admin', 'lfontes@fca.uncu.edu.ar', NULL);
-INSERT INTO apex_usuario_proyecto (proyecto, usuario_grupo_acc, usuario, usuario_perfil_datos) VALUES ('toba_usuarios', 'admin', 'toba', NULL);
