0d34b90c97a7e78f9a8147e29ed77f6ce79e21f2
Martin <mmolina@fca.uncu.edu.ar>
2024-07-26 13:55:55 -0300

    borrado del grafico de lineas

--- comision/php/ci_incio.php
+++ comision/php/ci_incio.php
@@ -163,38 +163,7 @@ class ci_incio extends comision_ci
 	//---- grafico ----------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------
 
-	function conf__grafico(comision_ei_grafico $grafico)
-	{/*
-
-		$j = count($this->s__datos);
-		for ($i = 0; $i < $j; $i++) {
-			list($horas, $minutos, $segundos) = explode(":", $this->s__datos[$i]['horas_trabajadas']);
-			$minu = intval($horas * 60) + (intval($minutos));
-			$datos_1[] = round($minu / 60, 2);
-			list($hora, $minuto, $segundos) = explode(":", $this->s__datos[$i]['horas_requeridad']);
-			$minut = intval($hora * 60) + (intval($minuto));
-			$datos_2[] = round($minut / 60, 2);
-			list($año, $mes, $dia) = explode("-", $this->s__datos[$i]['fecha']);
-			$dias[] = $dia; //.'/' . $mes;
-		}
-
-		$grafico->conf()->canvas__set_titulo('Horas Trabajadas');
-
-		$grafico->conf()
-			->serie__agregar('Horas Trabajadas', $datos_1)
-			->serie__set_color('green')
-			->serie__set_leyenda('Horas cumplidas');
-
-		$grafico->conf()
-			->serie__agregar('Horas Requeridas', $datos_2)
-			->serie__set_color('red')
-			->serie__set_leyenda('Horas Requeridas');
-
-		$serie = $grafico->conf()->serie('Horas Requeridas')->SetWeight(3);
-		$serie = $grafico->conf()->serie('Horas Trabajadas')->SetWeight(3);
-		$grafico->conf()->canvas()->xaxis->SetTickLabels($dias);
-		$grafico->conf()->canvas()->ygrid->SetFill(true, '#EFEFEF@0.8', '#BBCCFF@0.1');*/ 
-	}
+	
 	//-----------------------------------------------------------------------------------
 	//---- grafico barras----------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------
@@ -214,7 +183,7 @@ class ci_incio extends comision_ci
 			list($hora, $minuto, $segundos) = explode(":", $this->s__datos[$i]['horas_requeridad']);
 			$minut = intval($hora * 60) + (intval($minuto));
 			$datos_2[] = round($minut / 60, 2);
-			list($año, $mes, $dia) = explode("-", $this->s__datos[$i]['fecha']);
+			list($anio, $mes, $dia) = explode("-", $this->s__datos[$i]['fecha']);
 			$dias[] = $dia; //.'/' . $mes;
 		}
 
