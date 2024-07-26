e0db90010f5ada381faba261f863a71347d08f9e
Martin <mmolina@fca.uncu.edu.ar>
2024-07-26 12:52:38 -0300

    metadatos por agregar reloj

--- comision/metadatos/componentes/toba_ci/dump_2306.sql
+++ comision/metadatos/componentes/toba_ci/dump_2306.sql
@@ -86,6 +86,18 @@ INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objet
 );
 INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
 	'comision', --proyecto
+	'1192', --dep_id
+	'2306', --objeto_consumidor
+	'2312', --objeto_proveedor
+	'cuadrograf', --identificador
+	NULL, --parametros_a
+	NULL, --parametros_b
+	NULL, --parametros_c
+	NULL, --inicializar
+	NULL  --orden
+);
+INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
+	'comision', --proyecto
 	'1189', --dep_id
 	'2306', --objeto_consumidor
 	'2309', --objeto_proveedor
@@ -153,13 +165,20 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'comision', --proyecto
 	'1190', --pantalla
 	'2306', --objeto_ci
-	'1', --orden
+	'2', --orden
 	'1189'  --dep_id
 );
 INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
 	'comision', --proyecto
 	'1190', --pantalla
 	'2306', --objeto_ci
-	'2', --orden
+	'1', --orden
+	'1192'  --dep_id
+);
+INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
+	'comision', --proyecto
+	'1190', --pantalla
+	'2306', --objeto_ci
+	'3', --orden
 	'24234000021'  --dep_id
 );
