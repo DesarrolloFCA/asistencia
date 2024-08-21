911c57d57b0e5362a35c234e2f6bb4c5eecd43be
Martin <mmolina@fca.uncu.edu.ar>
2024-08-21 13:31:50 -0300

    Autorizaciones de Directores

--- comision/php/autorizaciones/ci_comisiones.php
+++ comision/php/autorizaciones/ci_comisiones.php
@@ -19,6 +19,8 @@ class ci_comisiones extends comision_ci
 				WHERE (pasada is null or pasada = false)
 				and catedra :: int in ((Select id_catedra from reloj.catedras_agentes a
 										where legajo = $legajo and jefe = true)) 
+				or id_departamento = ((Select id_departamento from reloj.departamento_director
+				where legajo_dir = $legajo))						
 				and legajo <> $legajo
 		Order by catedra, fecha, legajo ";
 			$datos = toba::db('comision')->consultar($sql);
