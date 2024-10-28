16a0d72f3c1327c8d14dea7a99e15a0943724a33
= <=>
2024-10-28 12:12:30 -0300

    eliminacion ei_arbol

--- ctrl_asis/php/datos/vistas_sanidad.php
+++ ctrl_asis/php/datos/vistas_sanidad.php
@@ -167,7 +167,6 @@ class vistas_sanidad extends toba_datos_relacion
 		$limit $offset";
 		if (count($where)>0) {
 			$sql1 = sql_concatenar_where($sql1, $where);
-			ei_arbol($sql1);
 		}
 		
 		$datos = toba::db('sanidad')->consultar($sql1);
