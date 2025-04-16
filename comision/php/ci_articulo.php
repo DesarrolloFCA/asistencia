ec1788049c37623d820e628201c8ff2c8e4619db
Martin Molina <mmolina@fca.uncu.edu.ar>
2025-04-16 14:13:36 -0300

    arreglo consultas de limites de dias por año

--- comision/php/ci_articulo.php
+++ comision/php/ci_articulo.php
@@ -634,7 +634,7 @@ class ci_articulo extends comision_ci
 							$sql = "SELECT count(*) cant FROM reloj.parte 
 								where id_motivo = $id_motivo
 								and legajo = $legajo
-								and anio = $anio";
+								and DATE_PART('year', fecha_inicio_licencia) = $anio";
 							$tomo = toba::db('comision')->consultar($sql);
 							if ($tomo[0]['cant'] <= 12) {
 								if ($datos['certificado'] <> null) {
@@ -659,7 +659,7 @@ class ci_articulo extends comision_ci
 							$sql = "SELECT count(*) cant FROM reloj.parte 
 								where id_motivo = $id_motivo
 								and legajo = $legajo
-								and anio = $anio";
+								and DATE_PART('year', fecha_inicio_licencia)= $anio";
 							$tomo = toba::db('comision')->consultar($sql);
 							if ($tomo[0]['cant'] <= 5) {
 								if ($datos['certificado'] <> null) {
@@ -786,7 +786,7 @@ class ci_articulo extends comision_ci
 							$sql = "SELECT count(*) cant FROM reloj.parte 
 								where id_motivo = $id_motivo
 								and legajo = $legajo
-								and anio = $anio";
+								and DATE_PART('year', fecha_inicio_licencia) = $anio";
 							$tomo = toba::db('comision')->consultar($sql);
 							if ($tomo[0]['cant'] <= 28) {
 								if ($datos['certificado'] <> null) {
