e5c25207a3a700da1cd9c50770b742582abb74a9
Martin Molina <mmolina@fca.uncu.edu.ar>
2025-05-20 11:35:51 -0300

    Arreglos de comisiones y periodicas o a realizar el mismo día

--- comision/metadatos/tablas/apex_fuente_datos_schemas.sql
+++ comision/metadatos/tablas/apex_fuente_datos_schemas.sql
@@ -10,6 +10,12 @@ INSERT INTO apex_fuente_datos_schemas (proyecto, fuente_datos, nombre, principal
 );
 INSERT INTO apex_fuente_datos_schemas (proyecto, fuente_datos, nombre, principal) VALUES (
 	'comision', --proyecto
+	'ctrl_asis', --fuente_datos
+	'reloj', --nombre
+	'0'  --principal
+);
+INSERT INTO apex_fuente_datos_schemas (proyecto, fuente_datos, nombre, principal) VALUES (
+	'comision', --proyecto
 	'mapuche', --fuente_datos
 	'sanidad', --nombre
 	'0'  --principal
